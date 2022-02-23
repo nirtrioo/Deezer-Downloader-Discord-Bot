ad1bbeddeeeb04b4c5011f4f617013487659a75d
nirtrioo <100270254+nirtrioo@users.noreply.github.com>
2022-02-23 15:31:32 +0800

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-# Add your bot token and your deezer token (arl)
+# kiyif75273@sueshaw.com
 
-token = ""
-arl = ""
+token = "ODg5NjgwOTg4NDkyNDE1MDM3.YUkx_Q.iLv7-8lmXcylkM99u1VDjCAKrfs"
+arl = "13674678de3306888963cb0357385d27b85131167393baca9e7e79f5a897a86375b4120fb4367b1056892cfd153cc6d6c44a3f4af0b4027b314fd36602e588904bd6f64585f268aaada55d1dbbc079b1df5849a46e02b9607cdeb1ce5db7c3fe"
